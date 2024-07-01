c767a11820aeebe0836332385ef06bc03a032841
Brian Chung <manwaibrianchung@gmail.com>
2024-07-01 22:54:23 +0800

    init project

---
+++ src/main/java/com/bcmw/flash_card/FlashCardApplication.java
@@ -0,0 +1,13 @@
+package com.bcmw.flash_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcmw/flash_card/FlashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcmw.flash_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
